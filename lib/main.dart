b09cb42d3c0542d3a9ce56b1a8232fdf66399666
Apple <kirtimngupta@gmail.com>
2021-09-13 22:48:21 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:provider/provider.dart';
-
+//kirti - Jaimit -Collab
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
